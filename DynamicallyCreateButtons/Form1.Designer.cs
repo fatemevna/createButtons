87e3327df2c5718b722016567c697787a24373bd
fatemevna <mohajerf43@gmail.com>
2024-08-31 12:09:03 +0330

    Project Created

---
+++ DynamicallyCreateButtons/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DynamicallyCreateButtons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(356, 234);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 22);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ DynamicallyCreateButtons/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicallyCreateButtons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBox.TextChanged += textBox_TextChanged;
+        }
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            var txtBox = sender as TextBox;
+            if (txtBox == null) return;
+        
+            var count = Convert.ToInt16(txtBox.Text);
+            //
+            var xPosition = 0;
+            for (var i = 1; i <= count; i++)
+            {
+                var button = new Button
+                {
+                    Tag = string.Format("Btn{0}", i),
+                    Text = string.Format("Button{0}", i),
+                    Location = new Point(xPosition, 20)
+                     
+
+                };// MessageBox.Show ($"button created");
+                xPosition = xPosition + 200;
+                Controls.Add(button);
+                if (i % 3 == 0) { button.BackColor = Color.Pink; button.ForeColor = Color.Red;
+                }
+                else if (i % 2 == 0) { button.BackColor = Color.AliceBlue; }
+                else if (i% 5 == 0) { button.BackColor= Color.YellowGreen; }
+                else { button.BackColor = Color.White; button.ForeColor = Color.YellowGreen; }
+                
+               // MessageBox.Show("button added to the form");
+            }
+
+            
+        }
+    } }
